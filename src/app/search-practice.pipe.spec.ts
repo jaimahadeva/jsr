64b06fc026424524e8eb4f3a157af810eef8daf7
mahadeva <mahadevb909@gmail.com>
2023-12-08 21:19:52 +0530

    replaced --

--- src/app/search-practice.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchPracticePipe } from './search-practice.pipe';
-
-describe('SearchPracticePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchPracticePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
